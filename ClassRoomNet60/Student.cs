4c65fffd9bec3d0c96c9825578d525f747df843f
miksezea <mikk289e@edu.zealand.dk>
2022-09-05 13:20:33 +0200

    commit1

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class Student
+    {
+        public string name { get; private set; }
+        public int birthMonth { get; private set; }
+        public int birthday { get; private set; }
+
+        public Student(string name, int birthMonth, int birthday)
+        {
+            this.name = name;
+            this.birthMonth = birthMonth;
+            this.birthday = birthday;
+        }
+    }
+}
